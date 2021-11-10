c77249622684ab8f87cbc660c5eca5fc41db5b5f
Dexl2000 <dexl386@gmail.com>
2021-11-11 00:42:54 +0800

    chenged

--- src/com/itproger/cures/Main.java
+++ src/com/itproger/cures/Main.java
@@ -1,13 +1,13 @@
 package com.itproger.cures;
 
 
-import java.sql.SQLException;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-
+        System.out.println("hello123");
 
     }
 
